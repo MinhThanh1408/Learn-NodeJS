6470482d5e6cfe75cd6fe8bb43eb0b7bdfccbf9c
Minh Thanh laptop <minhthanhlaptop@gmail.com>
2022-12-18 15:57:34 +0700

    add Prettier husky

---
+++ src/public/app/controllers/SiteController.js
@@ -0,0 +1,17 @@
+class SiteController{
+
+    //[GET] 
+    index(req, res){
+        res.render('home');
+    }
+
+    //[GET]/search
+    search(req, res){
+        res.send('Search');
+    }
+}
+
+
+module.exports = new SiteController;
+
+
